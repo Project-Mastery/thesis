ec986d68e26d838577465445073115c4125a3d03
RafaelCotrim <rafael.souza.cotrim@gmail.com>
2024-10-31 19:48:32 +0000

    feat: flake file

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    devShells.${system}.default= pkgs.mkShell
+    {
+      nativeBuildInputs = with pkgs; [
+        texliveFull
+        tex-fmt
+      ];
+    };
+  };
+}
